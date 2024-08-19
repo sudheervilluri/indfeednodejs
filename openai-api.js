766c43c98d676c965ad1450232d5015618b23a17
sudheervilluri <34118372+sudheervilluri@users.noreply.github.com>
2024-08-19 10:38:24 +0530

    Checkin

--- openai-api.js
+++ openai-api.js
@@ -7,7 +7,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 //  const prompt = "Write a story about a magic backpack.";
 
 async function generateContent(item) {
-    const prompt = `rewrite trending tweet ins hort and crisp in less than 300 charecters from ${item}.`;
+    const prompt = `rewrite trending tweet short and crisp in less than 300 charecters with trending tags from ${item}.`;
     const result = await model.generateContent(prompt);
     console.log(result.response.text());
     return result.response.text();
